0e17d52463a646a98466045deeb9654cc3510c71
André Michelle <andre.michelle@gmail.com>
2025-07-22 16:06:12 +0200

    fixes tests

--- packages/lib/dawproject/src/index.test.ts
+++ packages/lib/dawproject/src/index.test.ts
@@ -16,47 +16,47 @@ import {asInstanceOf} from "@opendaw/lib-std"
 
 describe("Serializer", () => {
     it("should serialize", () => {
-const project = Xml.element({
-    version: "1.0",
-    application: Xml.element({
-        name: "openDAW",
-        version: "0.1"
-    }, ApplicationSchema),
-    transport: Xml.element({
-        tempo: Xml.element({
-            unit: Unit.BPM,
-            value: 120
-        }, RealParameterSchema),
-        timeSignature: Xml.element({
-            nominator: 4,
-            denominator: 4
-        }, TimeSignatureParameterSchema)
-    }, TransportSchema),
-    structure: [
-        Xml.element({
-            id: "0",
-            contentType: "notes",
-            channel: Xml.element({
-                audioChannels: 2,
-                mute: Xml.element({value: true}, BooleanParameterSchema)
-            }, ChannelSchema),
-            tracks: [
+        const project = Xml.element({
+            version: "1.0",
+            application: Xml.element({
+                name: "openDAW",
+                version: "0.1"
+            }, ApplicationSchema),
+            transport: Xml.element({
+                tempo: Xml.element({
+                    unit: Unit.BPM,
+                    value: 120
+                }, RealParameterSchema),
+                timeSignature: Xml.element({
+                    nominator: 4,
+                    denominator: 4
+                }, TimeSignatureParameterSchema)
+            }, TransportSchema),
+            structure: [
                 Xml.element({
-                    id: "01",
-                    contentType: "audio"
+                    id: "0",
+                    contentType: "notes",
+                    channel: Xml.element({
+                        audioChannels: 2,
+                        mute: Xml.element({value: true}, BooleanParameterSchema)
+                    }, ChannelSchema),
+                    tracks: [
+                        Xml.element({
+                            id: "01",
+                            contentType: "audio"
+                        }, TrackSchema),
+                        Xml.element({
+                            id: "02",
+                            contentType: "audio"
+                        }, TrackSchema)
+                    ]
                 }, TrackSchema),
                 Xml.element({
-                    id: "02",
+                    id: "1",
                     contentType: "audio"
                 }, TrackSchema)
             ]
-        }, TrackSchema),
-        Xml.element({
-            id: "1",
-            contentType: "audio"
-        }, TrackSchema)
-    ]
-}, ProjectSchema)
+        }, ProjectSchema)
         const xml = Xml.pretty(Xml.toElement("Project", project))
         console.debug(xml)
         const result: ProjectSchema = Xml.parse(exampleXml, ProjectSchema)
